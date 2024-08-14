186102501988e8d867c51447c6fe3f087c914c5e
MAZAH11 <160371264+MAZAH11@users.noreply.github.com>
2024-08-14 12:12:17 -0400

    Update server.js

--- mainServer/server.js
+++ mainServer/server.js
@@ -5,20 +5,27 @@ const path = require('path');
 const app = express();
 
 // Proxy setup for Exercise 1
-app.use('/exercise1', createProxyMiddleware({ target: 'http://localhost:3000', changeOrigin: true }));
+app.use('/exercise1', createProxyMiddleware({ 
+    target: 'https://excercises123-1.onrender.com/exercise1', // Replace with actual domain
+    changeOrigin: true 
+}));
 
 // Proxy setup for Exercise 2
-app.use('/exercise2', createProxyMiddleware({ target: 'http://localhost:3010', changeOrigin: true }));
+app.use('/exercise2', createProxyMiddleware({ 
+    target: 'https://excercises123-1.onrender.com/exercise2', // Replace with actual domain
+    changeOrigin: true 
+}));
 
 // Proxy setup for Exercise 3
-app.use('/exercise3', createProxyMiddleware({ target: 'http://localhost:3020', changeOrigin: true }));
-
-// Proxy setup for Exercise 4
+app.use('/exercise3', createProxyMiddleware({ 
+    target: 'https://excercises123-1.onrender.com/exercise3', // Replace with actual domain
+    changeOrigin: true 
+}));
 
 
 // Serve a simple home page with links to each exercise
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname,'public', 'index.html'));
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 // Start the main server
