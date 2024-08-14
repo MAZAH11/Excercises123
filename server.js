0e024c6dbcc1470ac88d4c5fa050b8911ed8681c
MAZAH11 <160371264+MAZAH11@users.noreply.github.com>
2024-08-14 12:52:07 -0400

    Update server.js

--- server.js
+++ server.js
@@ -8,17 +8,17 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Route for Exercise 1
 app.get('/exercise1', (req, res) => {
-    res.sendFile(path.join(__dirname, 'exercise1','public', 'index.html'));
+    res.sendFile(path.join(__dirname, 'Exercise1','public', 'index.html'));
 });
 
 // Route for Exercise 2
 app.get('/exercise2', (req, res) => {
-    res.sendFile(path.join(__dirname, 'exercise2','public','index.html'));
+    res.sendFile(path.join(__dirname, 'Exercise2','public','index.html'));
 });
 
 // Route for Exercise 3
 app.get('/exercise3', (req, res) => {
-    res.sendFile(path.join(__dirname, 'exercise3','public',' index.html'));
+    res.sendFile(path.join(__dirname, 'Exercise3','public','index.html'));
 });
 
 // Home route
@@ -26,7 +26,7 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname,'public','index.html'));
 });
 
-const PORT = process.env.PORT || 3040;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
